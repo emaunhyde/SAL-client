a91fcbbe856e80dafbff98b0c1552ea4603544cc
emaunhyde <emaunaridnihyde@gmail.com>
2021-02-05 15:48:11 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import './App.css';
+import "./App.css";
+
+// component imports
 import Header from "./Components/Header.jsx";
-import Navbar from './Components/Navbar';
-import Searchbar from './Components/Searchbar';
-import CategoryGallery from './Components/CategoryGallery';
-import SearchContext from './Components/SearchContext.jsx';
-import Footer from './Components/Footer.jsx';
+import Navbar from "./Components/Navbar";
+import Searchbar from "./Components/Searchbar";
+import CategoryGallery from "./Components/CategoryGallery";
+import SearchContext from "./Components/SearchContext.jsx";
+import Footer from "./Components/Footer.jsx";
+import ProjectsGallery from "./Components/ProjectsGallery";
+import Project from "./Components/Project";
+
+// library functionality
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
 
 function App() {
+  const [searching, setSearching] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchString, setSearchString] = useState("");
+  const [searchComplete, setSearchComplete] = useState(false);
+
   return (
-
-    <div className="App">
-
-      <div>
-        <Header/>
-      </div>
-
-      <div>
-        <Navbar/>
-      </div>
-
-      <div>
-        <Searchbar/>
-      </div>
-
-      <div>
-        <CategoryGallery/>
-      </div>
-
-      <div>
-        <SearchContext/>
-      </div>
-
-      <div>
-        <Footer/>
-      </div>
-
-    </div>
-
+    <>
+      <Header />
+      <Navbar />
+      <SearchContext.Provider
+        value={{
+          searching,
+          setSearching,
+          searchResults,
+          setSearchResults,
+          searchString,
+          setSearchString,
+          searchComplete,
+          setSearchComplete,
+        }}
+      >
+        <Route exact path="/">
+          <Searchbar />
+          <CategoryGallery />
+        </Route>
+        <Route exact path="/projects">
+          <ProjectsGallery />
+        </Route>
+        <Route exact path="/project/:id" component={Project} />
+      </SearchContext.Provider>
+      <Footer />
+    </>
   );
 }
 
--- src/Components/SearchContext.jsx
+++ src/Components/SearchContext.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import React from "react";
 
-function SearchContext(props) {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const SearchContext = React.createContext();
 
 export default SearchContext;
